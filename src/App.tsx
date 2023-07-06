4d664483ef477012c07f80c3eb368fc123956ed8
Bao Nguyen <lnbnguyen43@gmail.com>
2023-07-06 20:13:41 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react";
+import MusicPlayer from "./BaiTap/Musicplayer";
 
 // import "./App.css";
-import BlockUi from "./Session2/BlockUi7";
-import Gallery from "./Examples/Gallery/Gallery";
-import Accordions from "./components/ButtonAccordions/Accordions";
-import Tab from "./components/ButtonTabs/Tab";
-import TabMusic from "./Session3/Musicplayer/TabMusic/TabMusic";
-import MusicPlayer from "./Session3/Musicplayer";
-import LoginForm from "./Session3/Form";
-import RegiterForm from "./Session3/Register";
 
 function App() {
   return (
     <div className="main">
       <MusicPlayer />
-      {/* <RegiterForm /> */}
-      {/* <BlockUi /> */}
     </div>
   );
 }
